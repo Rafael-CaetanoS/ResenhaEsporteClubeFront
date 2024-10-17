f1fe6c97346f53458b73e66d455c34e02c0e2ee2
Rafael Silva <t076576871@infraero.gov.br>
2024-10-17 17:26:42 -0300

    Validacao cadastro

--- FrontTcc/src/app/pages/cadastrar-atleta/cadastrar-atleta.component.ts
+++ FrontTcc/src/app/pages/cadastrar-atleta/cadastrar-atleta.component.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cadastrar-atleta',
   standalone: true,
-  imports: [RouterLink, NavbarComponent],
+  imports: [RouterLink, NavbarComponent, FormsModule, ReactiveFormsModule, CommonModule ],
   templateUrl: './cadastrar-atleta.component.html',
-  styleUrl: './cadastrar-atleta.component.css'
+  styleUrls: ['./cadastrar-atleta.component.css']
 })
 export class CadastrarAtletaComponent {
+  formCadastro!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.formCadastro = this.formBuilder.group({
+      Nome: ['', Validators.required],
+      Apelido: ['', Validators.required],
+      Data: ['', Validators.required],
+      Email: ['', [Validators.required, Validators.email]],
+      Telefone: ['', Validators.required],
+      Senha: ['', Validators.required],
+      ConfirmaSenha: ['', Validators.required],
+    });
+  }
+
+  postar() {
+    if (!this.formCadastro.valid) {
+      console.log('Formulário inválido');
+      return;
+    }
+    console.log('Formulário válido', this.formCadastro.value);
+  }
 }
