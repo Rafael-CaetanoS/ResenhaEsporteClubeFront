a6603f2e0ab8ee949ce179184eddcd7b3a3db757
Larissa Novais <larissanovais2003@gmail.com>
2024-11-28 01:19:00 -0300

    tratamento de erro para usuário já cadastrado e e-mail inválido

--- FrontTcc/src/app/pages/cadastrar-atleta/cadastrar-atleta.component.ts
+++ FrontTcc/src/app/pages/cadastrar-atleta/cadastrar-atleta.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import Swal from 'sweetalert2'; 
 import { NavbarComponent } from '../../components/navbar/navbar.component';
@@ -15,6 +15,7 @@ import { CadastrarAtletaService } from '../../services/cadastrar-atleta.service'
 })
 export class CadastrarAtletaComponent {
   formCadastro!: FormGroup;
+  erroCadastro: string | null = null;
 
   constructor(
     private service: CadastrarAtletaService,
@@ -25,7 +26,7 @@ export class CadastrarAtletaComponent {
       {
         nomeAtleta: ['', Validators.required],
         apelido: ['', Validators.required],
-        dataNascimento: ['', Validators.required],
+        dataNascimento: ['', Validators.required, this.dataPassadaValidator],
         email: ['', [Validators.required, Validators.email]],
         telefone: ['', Validators.required],
         senha: ['', Validators.required],
@@ -33,6 +34,14 @@ export class CadastrarAtletaComponent {
       },
       { validator: this.senhasIguaisValidator }
     );
+  }
+
+  dataPassadaValidator(control: AbstractControl): ValidationErrors | null {
+    const dataNascimento = new Date(control.value);
+    const hoje = new Date();
+    hoje.setHours(0, 0, 0, 0); // Ignorar horas para considerar apenas o dia
+
+    return dataNascimento > hoje ? { dataFutura: true } : null;
   }
 
   senhasIguaisValidator(formGroup: FormGroup) {
@@ -51,6 +60,8 @@ export class CadastrarAtletaComponent {
       next: (response) => {
         console.log('Cadastro realizado com sucesso:', response);
 
+        this.erroCadastro = null;
+
         Swal.fire({
           icon: 'success',
           title: 'Cadastro realizado!',
@@ -63,11 +74,22 @@ export class CadastrarAtletaComponent {
       error: (error) => {
         console.error('Erro ao cadastrar:', error);
 
-        Swal.fire({
-          icon: 'error',
-          title: 'Erro ao cadastrar',
-          text: 'Ocorreu um problema ao realizar o cadastro. Tente novamente mais tarde.',
-        });
+        if (error.status === 409) { 
+          Swal.fire({
+            icon: 'error',
+            title: 'Usuário já cadastrado!',
+            text: 'Ocorreu um problema ao realizar o cadastro. Tente inserir outro e-mail.',
+            confirmButtonText: 'OK',
+          });
+        } else if (error.status === 400 && error.error?.message === 'Email inválido') {
+          this.erroCadastro = 'E-mail inválido.';
+        } else {
+          Swal.fire({
+            icon: 'error',
+            title: 'Erro ao cadastrar',
+            text: 'Ocorreu um problema ao realizar o cadastro. Tente novamente mais tarde.',
+          });
+        }
       },
     });
   }
